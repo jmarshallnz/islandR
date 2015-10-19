0c0b334945bd0b47376b1bfb065c9aceed78e7d0
Jonathan Marshall <jmarshall@never.you.mind>
2015-10-19 15:33:55 +1300

    update mcmc to estimate independent p_{it}'s

--- R/island.R
+++ R/island.R
@@ -31,34 +31,44 @@ update_p = function(curr, humans, phi) {
   # proposal distribution
   logit_p_proposal_sigma_2 = 1
 
-  # update each of the logit_p's
   # TODO: Could do this at random instead
-  for (id in 1:length(curr$logit_p)) {
-    # update one of the logit_p's
-    logit_p = curr$logit_p;
-    logit_p[id] = rnorm(1, curr$logit_p[id], logit_p_proposal_sigma);
+  # for each covariate patterns
+  for (x in 1:nrow(curr$logit_p)) {
+    # we can optimise this quite a bit, as we need only update
+    # the log likelihood corresponding to this covariate pattern
+    # as every other param is staying the same
 
-    # inverse logit to new P
-    p = inverse_logit(logit_p)
+    h = humans[[x]]
 
-    # compute prior-hastings ratio
-    # Prior-Hastings ratio = Proposal(f,f')/Proposal(f',f) * Prior(f')/Prior(f)
-    # Proposal is normal distribution so is symmetric, so this drops down to the prior.
-    # Prior ratio exp((p^2-p'^2)/(2*sigma^2))
-    #
-    log_hastings_ratio = (curr$logit_p[id]^2-logit_p[id]^2)/(2*logit_p_proposal_sigma);
+    # update each of the logit_p's
+    for (id in 1:ncol(curr$logit_p)) {
+      logit_p = curr$logit_p[x,];
+      logit_p[id] = rnorm(1, curr$logit_p[x,id], logit_p_proposal_sigma);
 
-    # compute likelihood ratio
-    log_likelihood = log_lik(humans, phi, p)
-    log_likelihood_ratio = log_likelihood - curr$log_likelihood
+      # inverse logit to new P
+      p = inverse_logit(logit_p)
 
-    # accept/reject
-    log_alpha = log_likelihood_ratio + log_hastings_ratio
-    if (log_alpha > 0 || runif(1) < exp(log_alpha)) {
-      # accept new values, copy to old
-      curr$logit_p = logit_p
-      curr$log_likelihood = log_likelihood
-      curr$accept = curr$accept + 1
+      # compute prior-hastings ratio
+      # Prior-Hastings ratio = Proposal(f,f')/Proposal(f',f) * Prior(f')/Prior(f)
+      # Proposal is normal distribution so is symmetric, so this drops down to the prior.
+      # Prior ratio exp((p^2-p'^2)/(2*sigma^2))
+      #
+      log_hastings_ratio = (curr$logit_p[x,id]^2-logit_p[id]^2)/(2*logit_p_proposal_sigma);
+
+      # compute likelihood ratio
+      log_likelihood = log_lik(h, phi, p)
+      log_likelihood_ratio = log_likelihood - curr$log_likelihood[x]
+
+      # accept/reject
+      log_alpha = log_likelihood_ratio + log_hastings_ratio
+      if (log_alpha > 0 || runif(1) < exp(log_alpha)) {
+        # accept new values, copy to old
+        curr$logit_p[x,] = logit_p
+        curr$log_likelihood[x] = log_likelihood
+        curr$accept = curr$accept + 1
+      } else {
+        curr$reject = curr$reject + 1
+      }
     }
   }
   return(curr)
@@ -81,20 +91,23 @@ mcmc = function(humans, phi) {
   accept_reject = numeric(2)
 
   n_sources = ncol(phi)
+  n_times   = length(humans)
 
   # posterior
-  posterior_logit_p = matrix(NA, iterations/thinning, n_sources - 1)
+  posterior_logit_p = list()
 
   #' sample logit_p from the prior
-  logit_p = rnorm(n_sources-1, 0, logit_p_sigma)
+  logit_p = matrix(rnorm(n_times * (n_sources-1), 0, logit_p_sigma), n_times)
 
-  #' convert from logit_p to P
-  p = inverse_logit(logit_p)
-
-  log_likelihood = log_lik(humans, phi, p)
+  #' compute log-likelihood for each covariate pattern
+  log_likelihood = numeric(n_times)
+  for (x in 1:n_times) {
+    p = inverse_logit(logit_p[x])
+    log_likelihood[x] = log_lik(h[[x]], phi, p)
+  }
 
   # storage for the current iteration
-  curr = list(logit_p = logit_p, log_likelihood = log_likelihood, accept=0)
+  curr = list(logit_p = logit_p, log_likelihood = log_likelihood, accept=0, reject=0)
 
   # main MCMC loop
   post_i = 0;
@@ -109,9 +122,9 @@ mcmc = function(humans, phi) {
     }
     if (i > burnin && i %% thinning == 0) {
       post_i = post_i + 1;
-      posterior_logit_p[post_i,] = curr$logit_p
+      posterior_logit_p[[post_i]] = curr$logit_p
     }
   }
 
-  list(post = posterior_logit_p, ar = curr$accept / (iterations + burnin))
+  list(post = posterior_logit_p, ar = curr$accept / (curr$accept + curr$reject))
 }
