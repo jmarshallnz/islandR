ac03e5545fee784cf62d6b996dc1235ec3df9f88
Jonathan Marshall <jmarshall@never.you.mind>
2015-10-20 12:06:16 +1300

    update to use a matrix formulation of the model

--- R/island.R
+++ R/island.R
@@ -1,6 +1,7 @@
 #' Island model, metropolis-hastings fit on logit scale
 #'
 #' Eventually this will include AR1() model
+library(MASS)
 
 #' Log-likelihood function
 log_lik_R = function(humans, phi, p) {
@@ -28,28 +29,39 @@ inverse_logit = function(logit_p) {
 
 update_priors = function(curr) {
   # hyper-priors
-  alpha_mu   = 0
-  alpha_prec = 0.1
+  theta_0    = rep(0, length(curr$theta))
+  theta_prec = diag(0.1, length(curr$theta))
   tau_shape  = 0.1
   tau_rate   = 0.1
 
-  # first, our means
-  for (i in 1:ncol(curr$logit_p)) {
-    # posterior will be alpha_mu*alpha_prec + n * tau * mean(p) / alpha_prec + n * tau
-    mu_hat = mean(curr$logit_p[,i])
-    n      = nrow(curr$logit_p)
-    prec = alpha_prec + n * curr$tau
-    mu   = (alpha_mu*alpha_prec + n*mu_hat*curr$tau) / prec
-    curr$mu[i] = rnorm(1, mu, 1/sqrt(prec))
-  }
+  # 1. Sample theta
+  #
+  # prior theta ~ Normal(theta_0, theta_prec)
+  #
+  # and p ~iid Normal(X theta, tau)
+  #
+  # the posterior is
+  #
+  # theta ~ Normal(theta_hat, prec_hat)
+  #
+  # where
+  #
+  # prec_hat = theta_prec + tau X'X
+  # theta_hat = prec_hat^{-1} (theta_prec*theta_0 + tau X'y)
 
-  # update TAU
-  # posterior will be (tau_shape + n/2, tau_rate + sum(p - mu)^2/2)
-  rss = 0;
-  for (i in 1:ncol(curr$logit_p))
-    rss = rss + sum((curr$logit_p[,i] - curr$mu[i])^2)
+  var_hat   = solve(theta_prec + curr$tau * t(curr$X) %*% curr$X)
+  theta_hat = var_hat %*% (theta_prec %*% theta_0 + curr$tau * t(curr$X) %*% curr$p)
 
-  n     = nrow(curr$logit_p) * ncol(curr$logit_p)
+  curr$theta = mvrnorm(1, theta_hat, var_hat)
+
+  # 2. Sample tau
+  #
+  # prior tau ~ Gamma(tau_shape, tau_rate)
+  #
+  # posterior will be (tau_shape + n/2, tau_rate + (p - X theta)'(p - X theta)/2)
+  rss = sum((curr$p - curr$X %*% curr$theta)^2)
+
+  n     = length(curr$p)
   shape = tau_shape + n / 2
   rate  = tau_rate  + rss / 2
   curr$tau   = rgamma(1, shape, rate=rate)
@@ -60,48 +72,49 @@ update_priors = function(curr) {
 update_p = function(curr, humans, phi) {
 
   # proposal distribution
-  logit_p_proposal_sigma_2 = 1
+  p_proposal_sigma = 1
+
+  # first up, compute the fitted values
+  mu = curr$X %*% curr$theta
 
   # TODO: Could do this at random instead
   # for each covariate patterns
-  for (x in 1:nrow(curr$logit_p)) {
+  for (i in 1:length(curr$p)) {
+
     # we can optimise this quite a bit, as we need only update
     # the log likelihood corresponding to this covariate pattern
     # as every other param is staying the same
 
-    h = humans[[x]]
+    # find the covariate pattern corresponding to this observation
+    t = curr$t[i]
+    h = humans[[t]]
 
-    # update each of the logit_p's
-    for (id in 1:ncol(curr$logit_p)) {
-      logit_p = curr$logit_p[x,];
-      logit_p[id] = rnorm(1, curr$logit_p[x,id], logit_p_proposal_sigma);
+    # update the corresponding p
+    p     = curr$p
+    p[i]  = rnorm(1, curr$p[i], p_proposal_sigma);
 
-      # inverse logit to new P
-      p = inverse_logit(logit_p)
+    # compute prior-hastings ratio
+    # Prior-Hastings ratio = Proposal(f,f')/Proposal(f',f) * Prior(f')/Prior(f)
+    # Proposal is normal distribution so is symmetric, so this drops down to the prior.
 
-      # compute prior-hastings ratio
-      # Prior-Hastings ratio = Proposal(f,f')/Proposal(f',f) * Prior(f')/Prior(f)
-      # Proposal is normal distribution so is symmetric, so this drops down to the prior.
+    # Prior in our hierarchical model is determined by mu + tau
+    # exp(((p-mu)^2-(p'-mu)^2)/2*tau)
+    log_hastings_ratio = ((curr$p[i] - mu[i])^2 - (p[i] - mu[i])^2)*0.5*curr$tau;
 
-      # Prior in our hierarchical model is determined by mu + tau
-      # exp(((p-mu)^2-(p'-mu)^2)/2*tau)
-      log_hastings_ratio = ((curr$logit_p[x,id] - curr$mu[id])^2 -
-                            (logit_p[id] - curr$mu[id])^2)*0.5*curr$tau;
+    # compute likelihood ratio
+    p_new = p[curr$t == t]
+    log_likelihood = log_lik(h, phi, inverse_logit(p_new))
+    log_likelihood_ratio = log_likelihood - curr$log_likelihood[t]
 
-      # compute likelihood ratio
-      log_likelihood = log_lik(h, phi, p)
-      log_likelihood_ratio = log_likelihood - curr$log_likelihood[x]
-
-      # accept/reject
-      log_alpha = log_likelihood_ratio + log_hastings_ratio
-      if (log_alpha > 0 || runif(1) < exp(log_alpha)) {
-        # accept new values, copy to old
-        curr$logit_p[x,] = logit_p
-        curr$log_likelihood[x] = log_likelihood
-        curr$accept = curr$accept + 1
-      } else {
-        curr$reject = curr$reject + 1
-      }
+    # accept/reject
+    log_alpha = log_likelihood_ratio + log_hastings_ratio
+    if (log_alpha > 0 || runif(1) < exp(log_alpha)) {
+      # accept new values, copy to old
+      curr$p[i] = p[i]
+      curr$log_likelihood[t] = log_likelihood
+      curr$accept = curr$accept + 1
+    } else {
+      curr$reject = curr$reject + 1
     }
   }
   return(curr)
@@ -117,9 +130,6 @@ mcmc = function(humans, phi) {
   # priors
   logit_p_sigma = 1
 
-  # proposal
-  logit_p_proposal_sigma = 1
-
   # accept/reject
   accept_reject = numeric(2)
 
@@ -127,22 +137,34 @@ mcmc = function(humans, phi) {
   n_times   = length(humans)
 
   # posterior
-  posterior_logit_p = list()
+  posterior = list()
 
-  #' sample logit_p from the prior
-  logit_p = matrix(rnorm(n_times * (n_sources-1), 0, logit_p_sigma), n_times)
-  mu      = rep(0, n_sources - 1)
+  #' sample p from the prior
+  p = rnorm(n_times * (n_sources-1), 0, logit_p_sigma)
+
+  #' time column (in case time is important)
+  t = rep(1:n_times, 3)
+
+  #' design matrix
+  X = matrix(0, n_times * (n_sources-1), n_sources-1)
+  X[            1:n_times, 1] = 1
+  X[  n_times + 1:n_times, 2] = 1
+  X[2*n_times + 1:n_times, 3] = 1
+
+  #' parameter vector
+  theta   = rep(0, n_sources - 1)
+
+  #' precision
   tau     = 1
 
   #' compute log-likelihood for each covariate pattern
   log_likelihood = numeric(n_times)
   for (x in 1:n_times) {
-    p = inverse_logit(logit_p[x])
-    log_likelihood[x] = log_lik(h[[x]], phi, p)
+    log_likelihood[x] = log_lik(humans[[x]], phi, inverse_logit(p[t == x]))
   }
 
   # storage for the current iteration
-  curr = list(logit_p = logit_p, mu = mu, tau = tau, log_likelihood = log_likelihood, accept=0, reject=0)
+  curr = list(p = p, X = X, t = t, theta = theta, tau = tau, log_likelihood = log_likelihood, accept=0, reject=0)
 
   # main MCMC loop
   post_i = 0;
@@ -160,11 +182,11 @@ mcmc = function(humans, phi) {
     }
     if (i > burnin && i %% thinning == 0) {
       post_i = post_i + 1;
-      posterior_logit_p[[post_i]] = list(logit_p = curr$logit_p,
-                                         mu      = curr$mu,
-                                         tau     = curr$tau)
+      posterior[[post_i]] = list(p     = curr$p,
+                                 theta = curr$theta,
+                                 tau   = curr$tau)
     }
   }
 
-  list(post = posterior_logit_p, ar = curr$accept / (curr$accept + curr$reject))
+  list(post = posterior, ar = curr$accept / (curr$accept + curr$reject))
 }
