82e2305ef7bde3747f9533b8780d81fb26a8b25f
Jonathan Marshall <jmarshall@never.you.mind>
2017-11-07 10:08:35 +1300

    got the labelling wrong on A matrix in island model when dumping evolutionary parameters

--- R/sample_dist_island.R
+++ R/sample_dist_island.R
@@ -80,7 +80,7 @@ st_fit_island <- function(formula, sequences, non_primary = "Human", iters = 100
 
   # assign names to the evolution traces
   colnames(out$evolution) <- c("Iteration",
-                               apply(as.matrix(expand.grid("A",1:length(source_names),1:(length(source_names)+1))), 1, paste0, collapse=""),
+                               apply(as.matrix(expand.grid("A",1:(length(source_names)+1),1:length(source_names)))[,c(1,3,2)], 1, paste0, collapse=""),
                                paste0("R", 1:length(source_names)),
                                "Likelihood")
 
