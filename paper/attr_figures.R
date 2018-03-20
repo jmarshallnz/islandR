22ab6da8e9e98bd3fe49c92ad51987b5791fb202
Jonathan Marshall <jmarshall@never.you.mind>
2018-03-21 11:37:59 +1300

    updates attr_figures to use manawatu dataset from islandR

--- paper/attr_figures.R
+++ paper/attr_figures.R
@@ -1,48 +1,43 @@
 library(islandR)
 library(dplyr)
 library(tidyr)
-library(lubridate)
-library(forcats)
+library(ggplot2)
 
-manawatu <- read.csv("~/data/sa_report/data/extract_attribution.csv") %>%
-  mutate(SampledDate = as.Date(SampledDate), Year = year(SampledDate)) %>%
-  filter(Year >= 2005, Year <= 2014) %>%
-  mutate(Source4 = fct_collapse(Source, Poultry = c("Supplier A", "Supplier B", "Supplier Other", "Otherpoultry"),
-                                Ruminants = c("Cattle", "Sheep"),
-                                Water = "Environmental water",
-                                Other = c("Cat_dog_pet", "Duck_poultry", "Spent_hen", "Pig",
-                                          "Turkey", "Water_bird_wild", "Wild_bird_other"))) %>%
-  filter(Source4 != "Human" | !is.na(UR_num)) %>%
-  mutate(UR_fact = factor(UR_num))
+# filter out those where we don't have a location, and have a factor version
+# of UR2006_num
+dataset <- manawatu %>% filter(Source != "Human" | !is.na(UR2006_num)) %>%
+  mutate(UR2006_fact = factor(UR2006_num))
+
+num_samples = 1
 
 # Fit the sequence type distribution using the island model
-st_i = st_fit(formula = Source4 ~ ST,
+st_i = st_fit(formula = Source ~ ST,
          non_primary = "Human",
-         data = manawatu,
-         method="island",
+         data = dataset,
+         method = "island",
          sequences = ~ ASP + GLN + GLT + GLY + PGM + TKT + UNC,
-         samples = 1000)
+         samples = num_samples)
 
 # and using the Dirichlet
-st_d = st_fit(formula = Source4 ~ ST,
+st_d = st_fit(formula = Source ~ ST,
               non_primary = "Human",
-              data = manawatu,
-              method="dirichlet",
-              iters=1000)
+              data = dataset,
+              method = "dirichlet",
+              iters = num_samples)
 
 # do the attribution
-at_if <- attribution(ST ~ UR_fact, st_i, manawatu %>% filter(Source4 == "Human"))
-at_il <- attribution(ST ~ UR_num, st_i, manawatu %>% filter(Source4 == "Human"))
-at_df <- attribution(ST ~ UR_fact, st_d, manawatu %>% filter(Source4 == "Human"))
-at_dl <- attribution(ST ~ UR_num, st_d, manawatu %>% filter(Source4 == "Human"))
+at_if <- attribution(ST ~ UR2006_fact, st_i, manawatu %>% filter(Source == "Human"))
+at_il <- attribution(ST ~ UR2006_num, st_i, manawatu %>% filter(Source == "Human"))
+at_df <- attribution(ST ~ UR2006_fact, st_d, manawatu %>% filter(Source == "Human"))
+at_dl <- attribution(ST ~ UR2006_num, st_d, manawatu %>% filter(Source == "Human"))
 
-df_if <- predict(at_if, FUN=identity) %>% extract(X, into="UR_num", regex="UR_fact([-0-9]+)=1", convert=TRUE) %>%
+df_if <- predict(at_if, FUN=identity) %>% extract(X, into="UR2006_num", regex="UR2006_fact([-0-9]+)=1", convert=TRUE) %>%
   replace_na(replace=list(UR_num=-3)) %>% mutate(GenotypeModel = 'Island', AttributionModel='Categorical')
-df_il <- predict(at_il, FUN=identity) %>% extract(X, into="UR_num", regex="UR_num=([-0-9]+)", convert=TRUE) %>%
+df_il <- predict(at_il, FUN=identity) %>% extract(X, into="UR2006_num", regex="UR2006_num=([-0-9]+)", convert=TRUE) %>%
   mutate(GenotypeModel = 'Island', AttributionModel='Linear')
-df_df <- predict(at_df, FUN=identity) %>% extract(X, into="UR_num", regex="UR_fact([-0-9]+)=1", convert=TRUE) %>%
+df_df <- predict(at_df, FUN=identity) %>% extract(X, into="UR2006_num", regex="UR2006_fact([-0-9]+)=1", convert=TRUE) %>%
   replace_na(replace=list(UR_num=-3)) %>% mutate(GenotypeModel = 'Dirichlet', AttributionModel='Categorical')
-df_dl <- predict(at_dl, FUN=identity) %>% extract(X, into="UR_num", regex="UR_num=([-0-9]+)", convert=TRUE) %>%
+df_dl <- predict(at_dl, FUN=identity) %>% extract(X, into="UR2006_num", regex="UR2006_num=([-0-9]+)", convert=TRUE) %>%
   mutate(GenotypeModel = 'Dirichlet', AttributionModel='Linear')
 
 df <- bind_rows(df_if, df_il, df_df, df_dl)
@@ -51,17 +46,14 @@ save(list='df', file='attribution_fits.Rdata')
 load('attribution_fits.Rdata')
 
 plot_df <- df %>%
-  group_by(GenotypeModel, AttributionModel, Source, UR_num) %>% summarize(
+  group_by(GenotypeModel, AttributionModel, Source, UR2006_num) %>% summarize(
     m = mean(p),
     li = quantile(p, 0.1),
     ui = quantile(p, 0.9)
   ) %>% ungroup %>%
   mutate(Source = factor(Source, levels=rev(c("Other", "Water", "Ruminants", "Poultry"))))
 
-
 # plot
-library(ggplot2)
-
 cairo_pdf("attr_all.pdf", width=7, height=5)
 ggplot(plot_df) +
   geom_ribbon(aes(x=UR_num, ymin=li, ymax=ui, fill=Source), alpha=0.3) +
@@ -80,4 +72,3 @@ ggplot(plot_df) +
         axis.text.x = element_text(hjust=c(-0.1,1.1)),
         axis.text.y = element_text(vjust=c(-0.1,rep(0.5, 3), 1.1)))
 dev.off()
-
