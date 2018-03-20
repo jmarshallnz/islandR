d12c801e1035c806e265113fc82dd4dfe2125e94
Jonathan Marshall <jmarshall@never.you.mind>
2018-03-21 11:45:58 +1300

    reorder the UR2006_name factor correctly in the manawatu dataset

--- data-raw/process_data.R
+++ data-raw/process_data.R
@@ -4,5 +4,10 @@ library(devtools)
 base_path <- "data-raw"
 manawatu <- read.csv(file.path(base_path, "Manawatu.csv"))
 
+# reorder the UR2006_name factor
+library(dplyr)
+order <- manawatu %>% select(UR2006_num, UR2006_name) %>% unique %>% arrange(UR2006_num) %>% pull(UR2006_name)
+manawatu$UR2006_name = factor(manawatu$UR2006_name, levels=order)
+
 # save data within package
 devtools::use_data(manawatu, overwrite=TRUE)
