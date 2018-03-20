93778569161d08681738888ea6c047ef48f3b2f7
Jonathan Marshall <jmarshall@never.you.mind>
2018-03-21 12:00:20 +1300

    updates attr_figures some more

--- paper/attr_figures.R
+++ paper/attr_figures.R
@@ -8,7 +8,7 @@ library(ggplot2)
 dataset <- manawatu %>% filter(Source != "Human" | !is.na(UR2006_num)) %>%
   mutate(UR2006_fact = factor(UR2006_num))
 
-num_samples = 1
+num_samples = 10
 
 # Fit the sequence type distribution using the island model
 st_i = st_fit(formula = Source ~ ST,
@@ -23,20 +23,21 @@ st_d = st_fit(formula = Source ~ ST,
               non_primary = "Human",
               data = dataset,
               method = "dirichlet",
-              iters = num_samples)
+              samples = num_samples)
 
 # do the attribution
-at_if <- attribution(ST ~ UR2006_fact, st_i, manawatu %>% filter(Source == "Human"))
-at_il <- attribution(ST ~ UR2006_num, st_i, manawatu %>% filter(Source == "Human"))
-at_df <- attribution(ST ~ UR2006_fact, st_d, manawatu %>% filter(Source == "Human"))
-at_dl <- attribution(ST ~ UR2006_num, st_d, manawatu %>% filter(Source == "Human"))
+humans <- dataset %>% filter(Source == "Human")
+at_if <- attribution(ST ~ UR2006_fact, st_i, humans)
+at_il <- attribution(ST ~ UR2006_num, st_i, humans)
+at_df <- attribution(ST ~ UR2006_fact, st_d, humans)
+at_dl <- attribution(ST ~ UR2006_num, st_d, humans)
 
 df_if <- predict(at_if, FUN=identity) %>% extract(X, into="UR2006_num", regex="UR2006_fact([-0-9]+)=1", convert=TRUE) %>%
-  replace_na(replace=list(UR_num=-3)) %>% mutate(GenotypeModel = 'Island', AttributionModel='Categorical')
+  replace_na(replace=list(UR2006_num=-3)) %>% mutate(GenotypeModel = 'Island', AttributionModel='Categorical')
 df_il <- predict(at_il, FUN=identity) %>% extract(X, into="UR2006_num", regex="UR2006_num=([-0-9]+)", convert=TRUE) %>%
   mutate(GenotypeModel = 'Island', AttributionModel='Linear')
 df_df <- predict(at_df, FUN=identity) %>% extract(X, into="UR2006_num", regex="UR2006_fact([-0-9]+)=1", convert=TRUE) %>%
-  replace_na(replace=list(UR_num=-3)) %>% mutate(GenotypeModel = 'Dirichlet', AttributionModel='Categorical')
+  replace_na(replace=list(UR2006_num=-3)) %>% mutate(GenotypeModel = 'Dirichlet', AttributionModel='Categorical')
 df_dl <- predict(at_dl, FUN=identity) %>% extract(X, into="UR2006_num", regex="UR2006_num=([-0-9]+)", convert=TRUE) %>%
   mutate(GenotypeModel = 'Dirichlet', AttributionModel='Linear')
 
@@ -54,10 +55,10 @@ plot_df <- df %>%
   mutate(Source = factor(Source, levels=rev(c("Other", "Water", "Ruminants", "Poultry"))))
 
 # plot
-cairo_pdf("attr_all.pdf", width=7, height=5)
+pdf("fit_attribution.pdf", width=7, height=5)
 ggplot(plot_df) +
-  geom_ribbon(aes(x=UR_num, ymin=li, ymax=ui, fill=Source), alpha=0.3) +
-  geom_line(aes(x=UR_num, y=m, col=Source), lwd=1) +
+  geom_ribbon(aes(x=UR2006_num, ymin=li, ymax=ui, fill=Source), alpha=0.3) +
+  geom_line(aes(x=UR2006_num, y=m, col=Source), lwd=1) +
   facet_grid(GenotypeModel~AttributionModel) +
   scale_x_continuous(name=NULL, breaks=c(-3,3), labels=c("Rural", "Urban"), expand=c(0,0)) +
   scale_y_continuous(name="Percentage of cases", labels=scales::percent_format(), limits=c(0,1), expand=c(0,0)) +
