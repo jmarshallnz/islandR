3d0f6ff3dff4fb76fb8d482d55ef0c9d18571e03
Jonathan Marshall <jmarshall@never.you.mind>
2018-03-21 14:54:13 +1300

    adds R script for genotype figure

--- paper/fig_genotype_dist.R
+++ paper/fig_genotype_dist.R
@@ -4,31 +4,26 @@ library(tidyr)
 library(ggjoy)
 
 # Fit the sequence type distribution using the island model
-st_i = replicate(10, {
-  s=st_fit(formula = Source ~ ST,
-            non_primary = "Human",
-            data = manawatu,
-            method="island",
-            sequences = ~ ASP + GLN + GLT + GLY + PGM + TKT + UNC,
-            samples = 1000)
-  lapply(1:1000, function(i) { data.frame(ST=s$types, s$sampling_distribution[,,i], Iteration=i) })
-  }
-  )
+num_samples <- 10000
 
-# Reformat to a data.frame
-st_il <- apply(st_i, 2, function(x) { do.call(rbind, x) })
-st_il2 <- lapply(seq_along(st_il), function(x) { cbind(st_il[[x]], Chain=x)})
-st_id <- do.call(rbind, st_il2) %>% mutate(Iteration = Iteration + (Chain-1)*1000) %>%
-  select(-Chain)
+st_i = st_fit(formula = Source ~ ST,
+              non_primary = "Human",
+              data = manawatu,
+              method = "island",
+              sequences = ~ ASP + GLN + GLT + GLY + PGM + TKT + UNC,
+              samples=num_samples)
+
+st_il <- lapply(1:num_samples, function(i) { data.frame(ST=as.numeric(st_i$types), st_i$sampling_distribution[,,i], Iteration=i) })
+st_id <- do.call(rbind, st_il)
 
 # and using the Dirichlet
 st_d = st_fit(formula = Source ~ ST,
-            non_primary = "Human",
-            data = manawatu,
-            method="dirichlet",
-            iters=10000)
+              non_primary = "Human",
+              data = manawatu,
+              method = "dirichlet",
+              samples = num_samples)
 
-st_dl <- lapply(1:10000, function(i) { data.frame(ST=as.numeric(st_d$types), st_d$sampling_distribution[,,i], Iteration=i) })
+st_dl <- lapply(1:num_samples, function(i) { data.frame(ST=as.numeric(st_d$types), st_d$sampling_distribution[,,i], Iteration=i) })
 st_dd <- do.call(rbind, st_dl)
 
 # Join them up
@@ -38,15 +33,17 @@ st <- bind_rows(cbind(st_id, Model='Island'), cbind(st_dd, Model='Dirichlet')) %
 final <- st %>% group_by(Iteration, Model, ST) %>%
   mutate(P = P/sum(P)) %>% spread(Model, P)
 
-save(list='final', file='model_compare.Rdata')
-load('model_compare.Rdata')
+#save(list='final', file='model_compare.Rdata')
+#load('model_compare.Rdata')
 
+# Filter out the STs to plot
 sts <- c(403, 2343, 2026, 474)
 plot_dat <- final %>% filter(ST %in% sts) %>%
   gather(Model, P, Dirichlet, Island) %>% ungroup %>% mutate(ST = factor(ST, levels=sts, labels = paste0("ST-", sts)),
                                                              Scale = ifelse(Model == "Dirichlet" & ST %in% c("ST-403","ST-2343"), 2, 1.2))
 
-cairo_pdf("genotype_figure.pdf", width=7, height=4)
+# and do the plot
+pdf("fig_genotype_dist.pdf", width=7, height=4)
 ggplot(plot_dat) + geom_joy(aes(x=P, y=Source, fill=Model, scale=Scale), alpha=0.7, size=0.1, bandwidth=0.01) +
   facet_wrap(~ST, ncol=2) +
   ylab("") +
@@ -59,9 +56,4 @@ ggplot(plot_dat) + geom_joy(aes(x=P, y=Source, fill=Model, scale=Scale), alpha=0
         legend.margin = margin(1.5,3,3,3),
         legend.title = element_blank(),
         axis.text.x = element_text(hjust=c(-0.1,rep(0.5, 3), 1.1)))
-#        legend.background = element_rect(fill='transparent'))
-#        plot.margin = unit(rep(0.5, 4), units = 'cm'))
 dev.off()
-
-#       legend.key.height = unit(30, "points"))
-
