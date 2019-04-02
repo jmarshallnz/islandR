81659db2d5ef028b8157fa7f8945f07cc462e273
Jonathan Marshall <jmarshall@never.you.mind>
2019-04-02 13:05:27 +1300

    speed up a little bit by using P[j] as the (log-offset'd) value in the logdouble constructor, and add some notes on how we could speed it up more

--- src/covariate_model.cpp
+++ src/covariate_model.cpp
@@ -20,7 +20,11 @@ double log_lik(NumericMatrix humans, NumericMatrix phi, NumericVector p) {
     // calculate the likelihood for this human isolate
     std::vector<logdouble> lik_h(P.size());
     for (int j = 0; j < P.size(); j++) {
-      lik_h[j] = logdouble(phi(humans(h,0)-1, j), 1) * P[j];
+      // we could speed this up by doing the magic offset stuff first in phi,
+      // i.e. storing it such that the maximal exponent is factored out already.
+      // The P's are unlikely to be so small as to affect this very much then,
+      // so we could do it all with doubles, then log at the end
+      lik_h[j] = logdouble(phi(humans(h,0)-1, j), P[j]);
     }
     lik *= sum(lik_h) ^ humans(h,1);
   }
